d8fcdfe25daf1d6c12c79af037f42e78667e6610
arthatetuko <artha.tetuko@gmail.com>
2026-03-14 00:31:45 +0700

    fix dist routing

--- server.js
+++ server.js
@@ -20,24 +20,19 @@ const io=new Server(server,{
  transports:["websocket","polling"]
 })
 
-app.use(express.static("dist"))
+/* SERVE DIST BUILD */
+app.use(express.static(path.join(__dirname,"dist")))
 
-app.get("/",(req,res)=>{
- res.sendFile(path.join(__dirname,"index.html"))
-})
-
-app.get("/ping",(req,res)=>{
- res.send("pong")
+app.get("*",(req,res)=>{
+ res.sendFile(path.join(__dirname,"dist/index.html"))
 })
 
 io.on("connection",(socket)=>{
-
  console.log("player connected",socket.id)
 
  socket.on("disconnect",()=>{
   console.log("player disconnected")
  })
-
 })
 
 const PORT=process.env.PORT||3000
